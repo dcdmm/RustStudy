dcdc72cf988a68949527a68104320177c22551d0
dcdmm <dcdmmcomeon@gmail.com>
2023-01-05 17:33:18 +0800

    rust不同文件模块的进一步理解

--- demo/src/front_of_house.rs
+++ demo/src/front_of_house.rs
@@ -1,2 +1,18 @@
+// pub mod hosting{
+//     pub fn add_to_waitlist() {}
+// }
+// pub mod serving{
+//     pub mod serving {
+//         pub fn take_order() {}
+    
+//         pub fn serve_order() {}
+    
+//         pub fn take_payment() {}
+//     }
+// }
+
+// 与上等价
 pub mod hosting;
 pub mod serving;
+
+pub fn sleep() {}
--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,8 +1,26 @@
-mod front_of_house;
+// mod front_of_house {
+//     pub mod hosting{
+//         pub fn add_to_waitlist() {}
+//     }
+//     pub mod serving{
+//         pub mod serving {
+//             pub fn take_order() {}
+        
+//             pub fn serve_order() {}
+        
+//             pub fn take_payment() {}
+//         }
+//     }
 
+//     pub fn sleep() {}    
+// }
+mod front_of_house;  // 与上等价
+
+pub use front_of_house::sleep;
 pub use front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
+    sleep();
     hosting::add_to_waitlist();
     front_of_house::serving::serving::take_order();
     front_of_house::serving::serving::serve_order();
