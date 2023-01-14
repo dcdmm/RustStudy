347a00a76083480281a31574c775b8b55a9d0a68
dcdmm <dcdmmcomeon@gmail.com>
2023-01-14 23:41:41 +0800

    while循环学习

---
+++ b_basic/src/bin/while_loop.rs
@@ -0,0 +1,17 @@
+// while循环
+
+fn main() {
+    let mut number = 3;
+    while number !=0 {
+        println!("{number}!");
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index += 1;
+    }
+}
