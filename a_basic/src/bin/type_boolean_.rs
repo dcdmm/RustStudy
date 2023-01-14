2ccc97280938b72c5fbe2365f83c46c344f803e5
dcdmm <dcdmmcomeon@gmail.com>
2023-01-14 20:19:01 +0800

    布尔类型学习

---
+++ a_basic/src/bin/type_boolean_.rs
@@ -0,0 +1,50 @@
+// 布尔类型
+
+// The boolean type or bool is a primitive data type that can take on one of two values, called true and false.
+
+/*
+Logical not
+b	    !b
+true	false
+false	true
+
+Logical or
+a	    b	    a | b
+true	true	true
+true	false	true
+false	true	true
+false	false	false
+
+Logical and
+a	    b	    a & b
+true	true	true
+true	false	false
+false	true	false
+false	false	false
+
+Logical xor
+a	    b	    a ^ b
+true	true	false
+true	false	true
+false	true	true
+false	false	false
+
+Comparisons
+a	    b	    a == b
+true	true	true
+true	false	false
+false	true	false
+false	false	true
+
+Comparisons
+a	    b	    a > b
+true	true	false
+true	false	true
+false	true	false
+false	false	false
+*/
+fn main() {
+    let _x = true;
+    let y = false;
+    println!("{}", y)
+}
