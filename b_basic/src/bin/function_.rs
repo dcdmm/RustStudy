3da057482ebbd399ea0a8a5e14c6bac7c56ac409
dcdmm <dcdmmcomeon@gmail.com>
2023-01-15 10:47:44 +0800

    函数学习

---
+++ b_basic/src/bin/function_.rs
@@ -0,0 +1,14 @@
+// 函数
+
+// In function signatures, you must declare the type of each parameter.
+fn sum_i_and_j(i: i32, j:i32) -> i32 {
+    //  You can return early from a function by using the return keyword and specifying a value, but most functions return the last expression implicitly.
+    
+    // return i + j;
+    i + j  // 表达式
+}
+
+fn main() {
+    let result = sum_i_and_j(3, 5);
+    println!("result = {result}");
+}
