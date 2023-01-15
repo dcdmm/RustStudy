a4ce8da36720b9f346a30b9f1e6e2308d456d166
dcdmm <dcdmmcomeon@gmail.com>
2023-01-15 15:01:53 +0800

    元组学习

---
+++ c_basic/src/bin/tuple_.rs
@@ -0,0 +1,16 @@
+// 元组(Primitive Type tuple)
+
+/*
+A tuple is a general way of grouping together a number of values with a variety of types into one compound type. Tuples have a fixed length: once declared, they cannot grow or shrink in size.
+*/
+fn main() {
+    let tup = (500, 5.4, 1);
+
+    let (x, y, z) = tup;
+    println!("The value of x, y, z is: {x}, {y}, {z}");
+
+    // Tuples are a sequence. This means that they can be accessed by position
+    println!("{}", tup.0);
+    println!("{}", tup.1);
+    println!("{}", tup.2);
+}
