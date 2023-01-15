31a92dc776594cfad7411ea95b2f61dfb7dfa7b8
dcdmm <dcdmmcomeon@gmail.com>
2023-01-15 19:27:09 +0800

    切片学习

---
+++ c_basic/src/bin/type_slice_.rs
@@ -0,0 +1,21 @@
+// 切片(Primitive Type slice)
+
+fn main() {
+    let s = String::from("hello world");
+    let _s0: &str = &s[0..2];
+    let _s1 = &s[..2]; // 与上等价
+
+    let len = s.len();
+
+    let _ = &s[3..len];
+    let _ = &s[3..]; // 与上等价
+
+    let _ = &s[0..len];
+    let _ = &s[..]; // 与上等价
+
+    let a = [1, 2, 3, 4, 5];
+    // This slice has the type &[i32]. It works the same way as string slices do, by storing a reference to the first element and a length. 
+    let slice: &[i32] = &a[1..3];
+    let is_equal = slice == &[2, 3];
+    println!("{}", is_equal);
+}
