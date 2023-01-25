ff2835f3e3dc9d27cd204c789a96ef01c0b0cd6c
dcdmm <dcdmmcomeon@gmail.com>
2023-01-25 12:29:19 +0800

    字符串切片学习

--- c_basic/src/bin/type_str_.rs
+++ c_basic/src/bin/type_str_.rs
@@ -1,6 +1,7 @@
 // 字符串切片(Primitive Type str)
 
 // It is usually seen in its borrowed form, &str.
+// A &str is made up of two components: a pointer to some bytes, and a length. 
 fn main() {
     // String literals are string slices
     let _hello_world = "hello world!";
