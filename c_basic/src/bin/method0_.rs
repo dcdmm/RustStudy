a5f0f5937aab3ba402e66a89919f270448b78758
dcdmm <dcdmmcomeon@gmail.com>
2023-02-01 10:38:53 +0800

    方法的学习

--- c_basic/src/bin/method0_.rs
+++ c_basic/src/bin/method0_.rs
@@ -1,5 +1,45 @@
-// 
+// 方法
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // `self`指代此类型的实例(本例中self指代的是Rectangle结构体实例)
+    
+    // `self`的使用跟函数参数一样,要严格遵守Rust的所有权规则
+    // * `self`表示Rectangle的所有权转移到该方法中
+    // * `&self`表示该方法对Rectangle的不可变借用
+    // * `&mut self`表示可变借用
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 
 fn main() {
-    
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
---
+++ c_basic/src/bin/method1_.rs
@@ -0,0 +1,27 @@
+// Associated Functions(关联函数,含义类似python __new__(cls[, ...])魔法方法)
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    // The Self keywords in the return type and in the body of the function are aliases for the type that appears after the impl keyword, which in this case is Rectangle.
+    // 不将`self`作为第一个参数(因此不是方法,称为关联函数),故不能通过f.read()的方式调用
+    fn square(size: u32) -> Self { // 等价于:fn square(size: u32) -> Rectangle {
+        Self { // 等价于:Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    // To call this associated function, we use the :: syntax with the struct name; 
+    let sq = Rectangle::square(3);
+    println!("The area of the square is {} square pixels.", sq.area());
+}
