dbbe06d5c1bd05a3046b6c567027ec2eecd2382f
dcdmm <dcdmmcomeon@gmail.com>
2023-04-12 10:31:10 +0800

    学习了2个小方法

---
+++ z_std_string/src/bin/s_string_fn1_.rs
@@ -0,0 +1,21 @@
+// std::string::String
+/*
+方法学习:
+as_str / as_mut_str
+ */
+fn main() {}
+
+#[test]
+fn as_str_as_mut_str_fn() {
+    let s = String::from("foo");
+    // Extracts a string slice containing the entire String.
+    assert_eq!("foo", s.as_str());
+
+    let mut s = String::from("foobar");
+    // Converts a String into a mutable string slice.
+    let s_mut_str = s.as_mut_str();
+
+    s_mut_str.make_ascii_uppercase();
+
+    assert_eq!("FOOBAR", s_mut_str);
+}
