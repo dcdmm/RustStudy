82e9e32dc209aa7fced9465e6f5d42ff40cd5b97
dcdmm <dcdmmcomeon@gmail.com>
2023-04-12 09:54:10 +0800

    继续学习,come on!

--- c_project_demo/src/main.rs
+++ c_project_demo/src/main.rs
@@ -9,7 +9,7 @@ mod use_keyword1_;
 //     // as_public.rs文件内容
 // }
 // 与上等价
-// 文件夹作为一个模块(方式1、方式2不能混用):
+// 文件夹作为一个模块(注意:方式1、方式2不能混用):
 // 方式1. 该文件夹同级目录下创建一个与目录同名的rs文件(推荐)
 // 方式2. 该文件夹目录中创建一个mod.rs文件
 mod as_public;
@@ -18,7 +18,8 @@ mod as_public;
 use use_keyword0_::customer::eat_at_restaurant;
 use as_public::enums_public_::eat_at_restaurant as eat_at_restaurant_cat;
 
-use rand::Rng; // 第三方包
+// Adding rand as a dependency in Cargo.toml tells Cargo to download the rand package and any dependencies from crates.io and make rand available to our project.
+use rand::Rng; // 第三方库
 
 fn main() {
     eat_at_restaurant();
