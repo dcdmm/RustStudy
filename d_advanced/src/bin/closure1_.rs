dc43ff1589f69d81b906f2671dab3dc03c684d80
dcdmm <dcdmmcomeon@gmail.com>
2023-04-16 20:59:13 +0800

    闭包完成复习

--- d_advanced/src/bin/closure1_.rs
+++ d_advanced/src/bin/closure1_.rs
@@ -3,20 +3,14 @@
 fn main() {
     let x = 4;
 
+    // 报错:can't capture dynamic environment in a fn item:
     // fn equal_to_x(z: i32) -> bool {
     //     z == x
     // }
     // let _is_equal_func= equal_to_x(4);
-    // 程序运行结果:
-//     error[E0434]: can't capture dynamic environment in a fn item
-//  --> c_advanced\src\bin\closure1_.rs:7:14
-//   |
-// 7 |         z == x
-//   |              ^
-//   |
-//   = help: use the `|| { ... }` closure form instead
-    
-    // 闭包拥有一项函数所不具备的特性:获取作用域中的变量
-    let equal_to_x_c = |z| {z == x};
-    let _is_equal_closure = equal_to_x_c(4);
+
+    // 闭包拥有一项函数所不具备的特性:获取作用域中变量
+    let equal_to_x_c = |z| { z == x };
+    println!("{}", equal_to_x_c(4));
+    println!("{}", x);
 }
--- d_advanced/src/bin/closure2_.rs
+++ d_advanced/src/bin/closure2_.rs
@@ -1,104 +1,42 @@
 // 闭包(FnOnce/FnMut/Fn)
 
-// Closures can capture values from their environment in three ways, which directly map to the three ways a function can take a parameter: borrowing immutably, borrowing mutably, and taking ownership. The closure will decide which of these to use based on what the body of the function does with the captured values.
+/*
+Once a closure has captured a reference or captured ownership of a value from the environment where the closure is defined (thus affecting what, if anything, is moved into the closure), the code in the body of the closure defines what happens to the references or values when the closure is evaluated later (thus affecting what, if anything, is moved out of the closure).
+A closure body can do any of the following: move a captured value out of the closure, mutate the captured value, neither move nor mutate the value, or capture nothing from the environment to begin with.
 
-// The way a closure captures and handles values from the environment affects which traits the closure implements, and traits are how functions and structs can specify what kinds of closures they can use. Closures will automatically implement one, two, or all three of these Fn traits, in an additive fashion, depending on how the closure’s body handles the values:
 
-// FnOnce applies to closures that can be called once. All closures implement at least this trait, because all closures can be called. A closure that moves captured values out of its body will only implement FnOnce and none of the other Fn traits, because it can only be called once.
-// FnMut applies to closures that don’t move captured values out of their body, but that might mutate the captured values. These closures can be called more than once.
-// Fn applies to closures that don’t move captured values out of their body and that don’t mutate captured values, as well as closures that capture nothing from their environment. These closures can be called more than once without mutating their environment, which is important in cases such as calling a closure multiple times concurrently.
+The way a closure captures and handles values from the environment affects which traits the closure implements, and traits are how functions and structs can specify what kinds of closures they can use.
+ Closures will automatically implement one, two, or all three of these Fn traits, in an additive fashion, depending on how the closure’s body handles the values:
 
-// 创建闭包时,通过闭包对作用域变量的使用,Rust推断出具体使用哪个trait:
-// 1. 所有闭包都实现了FnOnce
-// 2. 没有获取作用域变量所有权的闭包实现了FnMut
-// 3. 不需要对作用域变量进行修改(即只需要获取作用域变量的不可变借用)的实现了Fn
-// ======>总结:Fn 的前提是实现FnMut,FnMut的前提是实现FnOnce
-// ======>总结:一个闭包实现了哪种Fn特征取决于该闭包如何使用被捕获的变量,而不是取决于闭包如何捕获它们
-
-fn fn_once<F>(func: F)
-where
-    // 获取作用域变量所有权
-    F: FnOnce(usize) -> bool,
-{
-    println!("{}", func(3));
-    // println!("{}", func(4));
-    // 程序运行结果:
-//     error[E0382]: use of moved value: `func`
-//   --> c_advanced\src\bin\closure2_.rs:15:20
-//    |
-// 18 | fn fn_once<F>(func: F)
-//    |               ---- move occurs because `func` has type `F`, which does not implement the `Copy` trait
-// ...
-// 23 |     println!("{}", func(3));
-//    |                    ------- `func` moved due to this call
-// 24 |     println!("{}", func(4));
-//    |                    ^^^^ value used here after move
-//    |
-// note: this value implements `FnOnce`, which causes it to be moved when called
-//   --> c_advanced\src\bin\closure2_.rs:14:20
-//    |
-// 23 |     println!("{}", func(3));
-//    |                    ^^^^
-// help: consider further restricting this bound
-//    |
-// 21 |     F: FnOnce(usize) -> bool + Copy,
-//    |                              ++++++
-}
-
-// 获取作用域变量的可变借用
-fn exec<'a, F: FnMut(&'a str)>(mut f: F) {
-    f("hello")
-}
-
-// 获取作用域变量的不可变借用
-fn exec1<'a, F: Fn(String) -> ()>(f: F)  {
-    f("world".to_string())
-}
-
+1. FnOnce applies to closures that can be called once. All closures implement at least this trait, because all closures can be called. A closure that moves captured values out of its body will only implement FnOnce and none of the other Fn traits, because it can only be called once.
+2. FnMut applies to closures that don’t move captured values out of their body, but that might mutate the captured values. These closures can be called more than once.
+3. Fn applies to closures that don’t move captured values out of their body and that don’t mutate captured values, as well as closures that capture nothing from their environment. These closures can be called more than once without mutating their environment, which is important in cases such as calling a closure multiple times concurrently.
+ */
 fn main() {
-    let x = vec![1, 2, 3];
-    fn_once(|z|{z == x.len()});
-    
-    // ********************************************
+    // we define a closure that captures an immutable reference to the vector named list because it only needs an immutable reference to print the value:
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    println!("************************************************************");
+    // we change the closure body so that it adds an element to the list vector. The closure now captures a mutable reference:
+    let mut list1 = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list1);
+
+    let mut borrows_mutably = || list1.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list1);
+
+    println!("************************************************************");
     let x1 = vec![1, 2, 3];
-    // 强制闭包获取作用域变量的所有权
-    let fn_once1 = move || x1;
+    let fn_once1 = move || x1; // 强制闭包获取作用域中变量的所有权
     println!("{:?}", fn_once1());
+    // 报错:borrow of moved value: `x1`
     // println!("{:?}", x1);
-    // 程序运行结果:
-//     error[E0382]: borrow of moved value: `x1`
-//     --> c_advanced\src\bin\closure2_.rs:54:22
-//      |
-//   63 |     let x1 = vec![1, 2, 3];
-//      |         -- move occurs because `x1` has type `Vec<i32>`, which does not implement the `Copy` trait
-//   64 |     // 强制闭包获取作用域变量的所有权
-//   65 |     let x1_move = move || x1;
-//      |                   ------- -- variable moved due to use in closure
-//      |                   |
-//      |                   value moved into closure here
-//   66 |     println!("{:?}", x1_move());
-//   67 |     println!("{:?}", x1);
-//      |                      ^^ value borrowed here after move
-
-    println!("************************************************");
-    let mut s = String::new();
-
-    // 想要在闭包内部获取作用域变量的可变借用,需要把该闭包声明为可变类型
-    let mut update_string =  |str| s.push_str(str);
-    update_string("hello");
-    println!("{:?}",s);
-
-    // ********************************************
-    let mut s1 = String::new();
-
-    let update_string1 =  |str| s1.push_str(str);
-    exec(update_string1);
-    println!("{:?}", s1);
-
-    println!("************************************************");
-    let m = "hello, ".to_string();
-
-    let print_string =  |str| println!("{},{}", m, str);
-    exec1(print_string);
-    println!("{:?}", m);
 }
