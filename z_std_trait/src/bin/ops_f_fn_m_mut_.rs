49cbe83a8900ea41a63367310a720f8f7d5b8c0b
dcdmm <dcdmmcomeon@gmail.com>
2023-04-26 11:46:38 +0800

    rust继续不断学习

--- z_std_trait/src/bin/ops_f_fn_m_mut_.rs
+++ z_std_trait/src/bin/ops_f_fn_m_mut_.rs
@@ -5,7 +5,6 @@ The version of the call operator that takes a mutable receiver.
 Instances of FnMut can be called repeatedly and may mutate state.
  */
 
-
 fn main() {
     // Calling a mutably capturing closure
     let mut s = String::new();
@@ -15,14 +14,15 @@ fn main() {
 
     // ******************************************************************
     // Using a FnMut parameter
-    fn do_twice<F: FnMut(&str) -> ()>(mut func: F)
+    // 闭包生命周期为'a(隐式推断;Lifetime Elision) <+++> FnMut(&'a str)参数生命周期也为'a ===> 防止悬垂引用
+    fn do_twice<'a, F: FnMut(&'a str) -> ()>(mut func: F)
     {
         func("hello");
         func("world");
     }
 
     let mut s1 = String::new();
-    let update_string1 = |pstr: &str| s1.push_str(pstr);
+    let update_string1 = |pstr| s1.push_str(pstr);
     do_twice(update_string1);
     println!("{}", s1);
 }
