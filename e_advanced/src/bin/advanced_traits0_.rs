33699b6bcb5c47cb4763ba2fb549568385815909
dcdmm <dcdmmcomeon@gmail.com>
2023-05-09 19:25:40 +0800

    高级trait学习

---
+++ e_advanced/src/bin/advanced_traits0_.rs
@@ -0,0 +1,66 @@
+// Fully Qualified Syntax for Disambiguation: Calling Methods with the Same Name
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("fly ===> Pilot");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("fly ===> Wizard");
+
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("fly ===> Human");
+    }
+}
+
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Dog")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("Animal")
+    }
+}
+
+
+fn main() {
+    let person = Human;
+    person.fly(); // fly ===> Human
+    Pilot::fly(&person); // fly ===> Pilot
+    Wizard::fly(&person); // fly ===> Wizard
+
+    /*
+    However, associated functions that are not methods don’t have a self parameter.
+    When there are multiple types or traits that define non-method functions with the same function name, Rust doesn't always know which type you mean unless you use fully qualified syntax.
+     */
+    println!("A baby dog is called a {}", Dog::baby_name());
+
+    // Using fully qualified syntax to specify that we want to call the baby_name function from the Animal trait as implemented on Dog
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+}
---
+++ e_advanced/src/bin/advanced_traits1_.rs
@@ -0,0 +1,20 @@
+// Using Supertraits to Require One Trait’s Functionality Within Another Trait
+
+use std::fmt;
+
+// Implementing the OutlinePrint trait that requires the functionality from Display
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        // Because we’ve specified that OutlinePrint requires the Display trait, we can use the to_string function that is automatically implemented for any type that implements Display.
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+fn main() {}
+
