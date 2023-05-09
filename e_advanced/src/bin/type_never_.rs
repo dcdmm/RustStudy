c0f3a70ed923d9a69102a232f6872920dea7324e
dcdmm <dcdmmcomeon@gmail.com>
2023-05-09 19:25:22 +0800

    never类型学习

---
+++ e_advanced/src/bin/type_never_.rs
@@ -0,0 +1,27 @@
+// The Never Type that Never Returns
+
+/*
+The ! type, also called “never”.
+
+! represents the type of computations which never resolve to any value at all. For example, the exit function fn exit(code: i32) -> ! exits the process without ever returning, and so returns !.
+
+break, continue and return expressions also have type !.
+ */
+fn main() {
+    let i = 2;
+
+    // 报错:`match` arms have incompatible types
+    // let _v = match i {
+    //     0..=3 => i,
+    //     _ => println!("不合规定的值:{}", i)
+    // };
+
+    let _v: u32 = match i {
+        0..=3 => i,
+        /*
+        Both match arms must produce values of type u32, but since break never produces a value at all we know it can never produce a value which isn’t a u32.
+        This illustrates another behaviour of the ! type - expressions with type ! will coerce into any other type.
+         */
+        _ => panic!("不合规定的值:{}", i)
+    };
+}
