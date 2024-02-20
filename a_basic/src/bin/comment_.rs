dfc6a34a089d9c31c71d7e4fe2dca86113eaa729
dcdmm <dcdmmcomeon@gmail.com>
2024-02-20 20:21:11 +0800

    怎么一个注释都这么多事情

--- a_basic/src/bin/comment_.rs
+++ a_basic/src/bin/comment_.rs
@@ -37,18 +37,3 @@ fn main() {
     */
     println!("hello rust!") // 单行注释
 }
-
-
-/// `add_one` 将指定值加1
-///
-/// # Examples11
-///
-/// ```
-/// let arg = 5;
-/// let answer = world_hello::compute::add_one(arg);
-///
-/// assert_eq!(6, answer);
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
-}
