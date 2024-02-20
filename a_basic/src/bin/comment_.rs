2174280f1e15e46963edca1338326c8d59fde9be
dcdmm <dcdmmcomeon@gmail.com>
2024-02-21 00:05:22 +0800

    注释学习

--- a_basic/src/bin/comment_.rs
+++ a_basic/src/bin/comment_.rs
@@ -1,37 +1,10 @@
-//! 计算一些你口算算不出来的复杂算术题
+// 代码注释
 
-
-/// `add_one`将指定值加1
-///
-
-
-// 注释
-
-/// 文档注释
-///
-/// # Panics
-///
-/// Panics if `index > len`.
-///
-/// # Examples
-///
-/// ```
-/// let mut vec = Vec::with_capacity(10);
-/// vec.extend([1, 2, 3]);
-/// assert!(vec.capacity() >= 10);
-/// vec.shrink_to_fit();
-/// assert!(vec.capacity() >= 3);
-/// ```
-///
-/// * `length` needs to be less than or equal to `capacity`.
-/// * The first `length` values must be properly initialized values of type `T`.
-/// * `capacity` needs to [*fit*] the layout size that the pointer was allocated with.
-/// * The allocated size in bytes must be no larger than `isize::MAX`.
 fn main() {
     // 单行注释
 
     /*
-    多行注释
+    多行注cc
     多行注释
     多行注释
     */
