4ef7e20d534afd0f78a8be21b159cabe4bc1c117
dcdmm <dcdmmcomeon@gmail.com>
2024-02-21 15:48:28 +0800

    常量复习

--- a_basic/src/bin/constant_.rs
+++ a_basic/src/bin/constant_.rs
@@ -1,7 +1,16 @@
 // 常量
 
 fn main() {
-    // Like immutable variables, constants are values that are bound to a name and are not allowed to change
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // 常量的定义(通过`const`关键字)
-    println!("{}", THREE_HOURS_IN_SECONDS)
+    println!("{}", THREE_HOURS_IN_SECONDS);
+
+    // Like immutable variables, constants are values that are bound to a name and are not allowed to change
+    // error[E0428]: the name `THREE_HOURS_IN_SECONDS` is defined multiple times
+    // note: `THREE_HOURS_IN_SECONDS` must be defined only once in the value namespace of this block
+    // const THREE_HOURS_IN_SECONDS: u32 = 410;
+
+    #[allow(warnings)]
+    let i: i32 = 60 * 60 * 3;
+    let i: i32 = 410;
+    println!("{}", i);
 }
