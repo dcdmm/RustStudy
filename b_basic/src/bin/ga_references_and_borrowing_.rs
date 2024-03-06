20fdfa7522159fcc84fe8191f5e9743cd52918f8
dcdmm <dcdmmcomeon@gmail.com>
2024-03-07 02:10:06 +0800

    问题解析

---
+++ b_basic/src/bin/ga_references_and_borrowing_.rs
@@ -0,0 +1,53 @@
+// 引用与借用问题解析 
+
+#[allow(warnings)]
+#[test]
+fn t0() {
+    let a = 34;
+    let b = &a;
+    let c = b; // 复制不可变引用(不可变引用可以有任意个)
+    println!("{a}, {b}, {c}");
+
+    let mut x = 41;
+    let y = &mut x;    
+    let z = y; // 移动可变引用(可变引用只能有一个)
+    // error[E0382]: borrow of moved value: `y`
+    // println!("{y}");
+}
+
+#[allow(warnings)]
+#[test]
+fn t1() {
+    let mut x = 41;
+    let y = &mut x;
+    // error[E0506]: cannot assign to `x` because it is borrowed
+    // x = 100;
+    println!("{y}");
+
+    let mut a = 34;
+    let b = &a;
+    // error[E0506]: cannot assign to `a` because it is borrowed
+    // a = 100;
+    println!("{b}");
+}
+
+#[allow(warnings)]
+#[test]
+fn t2() {
+    struct Point{
+        x: String,
+        y: i32,
+    }
+
+    let mut p = Point{x:String::from("hello"), y:3};
+    let pr = &mut p;
+    // error[E0507]: cannot move out of `pr.x` which is behind a mutable reference
+    // let x1 = pr.x;
+
+    let mut q = Point{x:String::from("hello"), y:3};
+    let qr = &q;
+    // error[E0507]: cannot move out of `qr.x` which is behind a shared reference
+    // let x1 = qr.x;
+}
+
+fn main() {}
--- b_basic/src/main.rs
+++ b_basic/src/main.rs
@@ -1,63 +1,17 @@
 fn main() {
+    struct Point{
+        x: String,
+        y: i32,
+    }
 
-    let mut a = 34;
-    let b = &mut a;
-    let c = b;
-    println!("{b}");
-    // println!("{c}");
-    // let number = 10;
-    // let some_number = Some(number);
+    let mut p = Point{x:String::from("hello"), y:3};
+    let pr = &p;
+    let prr = &pr;
+    println!("{}", prr.x);
 
-    // match &&&&&&some_number {
-    //     &&&&&&Some(s) => {
-    //         println!("{}", std::any::type_name_of_val(&s));
-    //     },
-    //     None => println!("没有匹配到任何数字"),
-    // }
+    // let mut p1 = Point{x:String::from("hello"), y:3};
 
-    // struct p{
-    //     x: String,
-    //     y: i32,
-    // }
-
-    // let mut  p0 = p{x:String::from("hello"), y:3};
-    // let p0r =  &mut p0;
-    // let x1 = p0.x;
-    // println!("{}", p0.x);
-
-    // // let qq = String::from("value");
-    // // let qqr = &qq;
-    // // let y2 = *qqr;
-    // // println!("{}", qqr);
-
-    // println!("{}", 34);
-
-    // let a1:i32;
-    // let b = &a1;
-
-    // let s1 = String::from("3");
-    // let s2 = &s1;
-    // let q = s1;
-    // println!("{}", s2)
-
-    // println!("{}", p0.x);
-    // println!("{}", (*p0r).x);
-    // let int_reference = &3;
-
-    // let a = match *int_reference {
-    //     0 => "zero",
-    //     _ => "some",
-    // };
-    // let b = match int_reference {
-    //     &0 => "zero",
-    //     _ => "some",
-    // };
-
-    // let a = 234;
-    // let b = &a;
-    // let c  = &b;
-    // let d = &c;
-    // println!("{}", std::any::type_name_of_val(&a));
-    // println!("{}", std::any::type_name_of_val(&b));
-    // println!("{}", std::any::type_name_of_val(&d));
+    // let m = (p, 1);
+    // let ms = &m;
+    // println!("{}", (*ms).0.x);
 }
