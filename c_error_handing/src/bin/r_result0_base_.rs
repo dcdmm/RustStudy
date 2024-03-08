23c936406c87bc3a1fb4d42b25459504f709f828
dcdmm <dcdmmcomeon@gmail.com>
2024-03-08 15:15:33 +0800

    太难了呀.啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊

--- c_error_handing/src/bin/r_result0_base_.rs
+++ c_error_handing/src/bin/r_result0_base_.rs
@@ -1,51 +1,45 @@
 // Recoverable Errors with Result
 
 /*
-Error handling with the Result type.
-
-Result<T, E> is the type used for returning and propagating errors.
-It is an enum with the variants, Ok(T), representing success and containing a value, and Err(E), representing error and containing an error value.
-
+Result<T, E> is the type used for returning and propagating errors. It is an enum with the variants, Ok(T), representing success and containing a value, and Err(E), representing error and containing an error value.
+```rust
 enum Result<T, E> {
-   Ok(T),
-   Err(E),
+    Ok(T),
+    Err(E),
 }
+```
  */
-
 use std::fs::File;
 use std::io::ErrorKind;
 
 #[allow(warnings)]
-fn main() {
-    // let greeting_file_result: Result<T, E> = File::open("hello.txt");
-    //
-    // // panic:thread 'main' panicked at 'Problem opening the file: Os { code: 2, kind: NotFound, message: "系统找不到指定的文件。" }'
-    // let greeting_file = match greeting_file_result {
-    //     /*
-    //     When the result is Ok, this code will return the inner file value out of the Ok variant, and we then assign that file handle value to the variable greeting_file.
-    //     After the match, we can use the file handle for reading or writing.
-    //      */
-    //     Ok(file) => file,
-    //     // The other arm of the match handles the case where we get an Err value from File::open. In this example, we’ve chosen to call the panic! macro.
-    //     Err(error) => panic!("Problem opening the file: {:?}", error),
-    // };
+#[test]
+fn t0() {
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        // When the result is Ok, this code will return the inner file value out of the Ok variant, and we then assign that file handle value to the variable greeting_file. After the match, we can use the file handle for reading or writing.
+        Ok(file) => file,
+        // The other arm of the match handles the case where we get an Err value from File::open. In this example, we’ve chosen to call the panic! macro.
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+}
 
-    // *****************************************************************************
+#[allow(warnings)]
+// #[test]
+fn main() {
     let greeting_file_result = File::open("hello.txt");
 
-    // Matching on Different Errors
+    // Handling different kinds of errors in different ways
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) =>
-            match error.kind() {
-                ErrorKind::NotFound =>
-                    match File::create("hello.txt") {
-                        Ok(fc) => fc,
-                        Err(e) => panic!("Problem creating the file: {:?}", e),
-                    }
-                other_error => {
-                    panic!("Problem opening the file: {:?}", other_error);
-                }
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
             },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
     };
 }
