e137a0ca2f7fa924141fdd5741252528cdd10e17
dcdmm <dcdmmcomeon@gmail.com>
2024-03-08 14:49:06 +0800

    panci学习

--- c_error_handing/src/bin/panic_.rs
+++ c_error_handing/src/bin/panic_.rs
@@ -1,8 +1,10 @@
 // Unrecoverable Errors with panic!
 
+#[test]
+fn t0() {
+    panic!("crash and burn");  // 显式调用panic!宏(主动触发,类似使用raise关键字主动触发异常)
+}
 fn main() {
-    // panic!("crash and burn"); // Panics the current thread.(类似python raise关键字的作用)
-
     let v = vec![1, 2, 3];
-    v[99];
+    v[99]; // 数组越界访问
 }
