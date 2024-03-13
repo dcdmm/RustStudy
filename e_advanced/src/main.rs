6326cd874de1b8c41d33a5ca584c18dc3053bf06
dcdmm <dcdmmcomeon@gmail.com>
2024-03-13 16:19:59 +0800

    come on

--- e_advanced/src/main.rs
+++ e_advanced/src/main.rs
@@ -1,12 +1,5 @@
 mod lifetime;
 
 fn main() {
-    let r;          // ---------+-- 'a
-                          //          |
-    {                     //          |
-        let x = 5;   // -+-- 'b  |
-        r = &x;           //  |       |
-    }                     // -+       |
-                          //          |
-    println!("r: {}", r); //          |
+    println!("Hello, world!");
 }
