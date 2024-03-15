31e189d53d1598f8fe37706b6ef1276382c1a3d3
dcdmm <dcdmmcomeon@gmail.com>
2024-03-15 09:57:01 +0800

    结构调整

---
+++ f_advanced/src/closure/closure1_.rs
@@ -0,0 +1,53 @@
+// 闭包
+
+/*
+Closures can capture values from their environment in three ways, which directly map to the three ways a function can take a parameter: borrowing immutably, borrowing mutably, and taking ownership. The closure will decide which of these to use based on what the body of the function does with the captured values.
+*/
+
+#[test]
+fn t0() {
+    let x = 4;
+
+    // fn equal_to_x(z: i32) -> bool {
+    //     // error[E0434]: can't capture dynamic environment in a fn item
+    //     z == x
+    // }
+    // let _is_equal_func = equal_to_x(4);
+
+    let equal_to_x = |z| z == x;
+    let _is_equal_closure = equal_to_x(4);
+}
+
+#[test]
+fn only_borrows_() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list); // 捕获不可变借用
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+}
+
+#[test]
+fn borrows_mutably_() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7); // 捕获可变借用
+
+    // error[E0502]: cannot borrow `list` as immutable because it is also borrowed as mutable
+    // println!("Before calling closure: {:?}", list);
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+}
+
+#[test]
+fn take_owership_() {
+    let list = vec![1, 2, 3]; // `list`: move occurs because `list` has type `Vec<i32>`, which does not implement the `Copy` trait
+    let take_owership = move || list; // `move ||`: value moved into closure here
+    take_owership();
+    // error[E0382]: borrow of moved value: `list`
+    // println!("{:?}", list);
+}
