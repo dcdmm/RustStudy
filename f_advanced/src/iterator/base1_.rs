f1747286ef9f2545a8422d6fc001c17d890c69b8
dcdmm <dcdmmcomeon@gmail.com>
2024-03-27 15:32:00 +0800

    继续前行

--- f_advanced/src/iterator/base1_.rs
+++ f_advanced/src/iterator/base1_.rs
@@ -2,5 +2,22 @@
 
 #[test]
 fn hash_set_test() {
+    // std::ops::Range
+    for i in 1..3 { 
+        print!("{}\t", i)
+    }
+    println!();
 
+    // std::ops::RangeInclusive
+    for i in 1..=3 {
+        print!("{}\t", i)
+    }
+
+    // Option<T>
+    let s = Some(10);
+    let _sii = s.into_iter();
+
+    // Result<T, E>
+    let r: Result<u32, &str> = Ok(2);
+    let _rii = r.into_iter();
 }
